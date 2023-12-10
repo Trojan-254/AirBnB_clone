3e1a9aaefc071979ec47bf953b2e93ca15df10d8
Trojan-254 <samwuels254@gmail.com>
2023-12-10 10:07:27 -0800

    Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,28 @@ class BaseModel():
     """The base model of all the classes"""
 
     def __init__(self, *args, **kwargs):
-        """The class initializer"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        """The class initializer
+
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-value arguments
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+
+                else:
+                    self.__dict__[key] = kwargs[key]
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """Returns a string representation of aclass object"""
